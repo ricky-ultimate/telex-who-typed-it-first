7b41936ff78a241a2d763ccb90f6af4919361305
Ricky Ultimate <rickyogigi@gmail.com>
2025-02-22 13:51:17 +0100

    feat(app): setup Express server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import { ENV } from "./constants/env";
+import logger from "./utils/logger.utils";
+import speedGameRoutes from "./core/speed-game/speed-game.routes";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+
+app.use("/api/speed-game", speedGameRoutes);
+
+app.listen(ENV.PORT, () => {
+    logger(`🚀 Server running on port ${ENV.PORT}`);
+});
